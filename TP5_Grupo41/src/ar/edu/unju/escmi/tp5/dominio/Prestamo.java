ea5f02602c32d01c34e549efe99f6296f4affb01
chano <chano@LAPTOP-VDHN2S85>
2024-10-01 20:44:48 -0300

    Parte 3 del menu finalizada. Reinoso Luciano.

---
+++ TP5_Grupo41/src/ar/edu/unju/escmi/tp5/dominio/Prestamo.java
@@ -0,0 +1,33 @@
+package ar.edu.unju.escmi.tp5.dominio;
+
+import java.time.LocalDate;
+
+public class Prestamo {
+	protected String id;
+	protected LocalDate fechaPrestamo;
+	protected LocalDate fechaDevolucion;
+	protected Libro libro;
+	protected Usuario usuario;
+	
+    public Prestamo(String id, LocalDate fechaPrestamo, LocalDate fechaDevolucion, Libro libro, Usuario usuario) {
+		super();
+		this.id = id;
+		this.fechaPrestamo = fechaPrestamo;
+		this.fechaDevolucion = null;
+		this.libro = libro;
+		this.usuario = usuario;
+	}
+
+	public void registrarDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+        this.libro.setEstado(true);
+    }
+    
+    public void mostrarDatos() {
+        System.out.println("ID del préstamo: " + id);
+        System.out.println("Fecha de préstamo: " + fechaPrestamo);
+        System.out.println("Fecha de devolución: " + (fechaDevolucion != null ? fechaDevolucion : "No devuelto aun"));
+        System.out.println("Libro: " + libro.getTitulo());
+        System.out.println("Usuario: " + usuario.getApellido() + ", " + usuario.getNombre() + ".");
+    }
+}
